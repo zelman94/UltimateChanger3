ccf5fd9137f54d079b9fbed46d1d2d1a1c53072d
zelman94 <paw94@op.pl>
2018-08-25 19:34:24 +0200

    odczyt i ustawieniie default

--- ReleaseApp/myXMLReader.cs
+++ ReleaseApp/myXMLReader.cs
@@ -10,7 +10,31 @@ namespace UltimateChanger
     class myXMLReader
     {
 
+        public SortedDictionary<string, string> getDefaultSettings()
+        {
+            //pobieram z XML ustawienia default
+            //dodaje do listy / macierz ustawienie i wartość
+            //zwracam
+            //przez slownik wartosc xml => kontrolka nadaje odpowiednie ustawienia
+            SortedDictionary<string, string> StringToUI = new SortedDictionary<string, string>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Settings\\Defaults.xml");
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                StringToUI.Add(node.Name, node.InnerText);              
 
+            }
+            return StringToUI;
+        }
+        public void setSetting(string tmp)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Settings\\Defaults.xml");
+            XmlNode root = doc.DocumentElement;
+            XmlNode myNode = root.SelectSingleNode($"{tmp}::Value");
+            myNode.Value = "blabla";
+            doc.Save("D:\\build.xml");
+        }
         
 
 
