4efe3c14421884b41aa4d1cb1a18dfd83e04e7b8
zelman94 <paw94@op.pl>
2018-10-30 21:14:22 +0100

    wątki dla install FS
    sprawdzić czy działa

--- ReleaseApp/FSInstaller.cs
+++ ReleaseApp/FSInstaller.cs
@@ -5,9 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
+using System.Threading;
 
 namespace UltimateChanger
 {
+
+    class Install_
+    {
+        public void Start_Installation(object path)
+        {
+            try
+            {
+                Process.Start(path.ToString());
+            }
+            catch (Exception)
+            {
+
+            }
+           
+        }
+        public void Start_Silent_Installation(object path)
+        {
+            try
+            {
+                Process.Start(path.ToString(), " /uninstall /quiet");
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
     public class FSInstaller
     {
         public bool InstallBrand(string path, bool mode_normal)
@@ -15,8 +44,14 @@ namespace UltimateChanger
             if (File.Exists(path) && mode_normal)
             {
                 try
-                {
-                    Process.Start(path);
+                { // dodać wątek 
+                    Install_ THInstall = new Install_();
+                    Thread oThread = new Thread(new ParameterizedThreadStart(
+                    THInstall.Start_Installation));
+                    oThread.Start(path);
+                    oThread.Join();
+
+                    //Process.Start(path);
                     return true;
                 }
                 catch (Exception)
@@ -28,8 +63,16 @@ namespace UltimateChanger
             {
                 try
                 {
+                    // dodać wątek 
                     //string tmp2 = @"\\10.128.3.1\DFS_Data_SSC_FS_GenieBuilds\Phoenix\ExpressFit\ExpressFit_4.0.784.161\Full\Sonic\Setup.exe /quiet";
-                    Process.Start(path, " /uninstall /quiet");
+                    Install_ THInstall = new Install_();
+                    Thread oThread = new Thread(new ParameterizedThreadStart(
+                    THInstall.Start_Silent_Installation));
+                    oThread.Start(path);
+                    oThread.Join();
+
+
+                   // Process.Start(path, " /uninstall /quiet");
                     return true;
                 }
                 catch (Exception)
