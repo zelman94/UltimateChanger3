9707598f649237db1aac179ddc79e1b42895c38f
zelman94 <paw94@op.pl>
2019-04-04 08:15:43 +0200

    new window

---
+++ ReleaseApp/Nightly_upgrade_FS.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UltimateChanger
+{
+    /// <summary>
+    /// Interaction logic for Nightly_upgrade_FS.xaml
+    /// </summary>
+
+    public partial class Nightly_upgrade_FS : Window
+    {
+        List<Label> lableListForUi = new List<Label>();
+        List<TextBox> listTextBoxForUi = new List<TextBox>();
+        List<Button> buttonListForUi = new List<Button>();
+        public Nightly_upgrade_FS()
+        {
+            InitializeComponent();
+            foreach (Label tb in FindLogicalChildren<Label>(this)) // dziala
+            {
+                lableListForUi.Add(tb);
+            }
+
+            foreach (TextBox item in FindLogicalChildren<TextBox>(this))
+            {
+                listTextBoxForUi.Add(item);
+            }
+            foreach (Button item in FindLogicalChildren<Button>(this))
+            {
+                buttonListForUi.Add(item);
+            }
+
+            //USTAWIENIA LABELI
+            foreach (var item in lableListForUi)
+            {
+                item.Foreground = ((MainWindow)System.Windows.Application.Current.MainWindow).lblSavedTime.Foreground;
+            }
+
+            //USTAWIENIA BOXÓW
+            foreach (var item in listTextBoxForUi)
+            {
+                item.Foreground = ((MainWindow)System.Windows.Application.Current.MainWindow).txtnewTeamMember.Foreground;
+                item.BorderBrush = ((MainWindow)System.Windows.Application.Current.MainWindow).txtnewTeamMember.BorderBrush;
+            }
+            var converter = new System.Windows.Media.BrushConverter();
+
+            //USTAWIENIA BUTTONÓW
+            foreach (var item in buttonListForUi)
+            {
+                item.Background = ((MainWindow)System.Windows.Application.Current.MainWindow).txtnewTeamMember.Background;
+                item.Foreground = ((MainWindow)System.Windows.Application.Current.MainWindow).txtnewTeamMember.Foreground;
+                item.BorderBrush = ((MainWindow)System.Windows.Application.Current.MainWindow).txtnewTeamMember.BorderBrush;
+                item.Opacity = ((MainWindow)System.Windows.Application.Current.MainWindow).txtnewTeamMember.Opacity;
+                item.MaxWidth = ((MainWindow)System.Windows.Application.Current.MainWindow).txtnewTeamMember.MaxWidth;
+            }
+            this.Background = ((MainWindow)System.Windows.Application.Current.MainWindow).Background;
+        }
+        public static IEnumerable<T> FindLogicalChildren<T>(DependencyObject obj) where T : DependencyObject
+        {
+            if (obj != null)
+            {
+                if (obj is T)
+                    yield return obj as T;
+
+                foreach (DependencyObject child in LogicalTreeHelper.GetChildren(obj).OfType<DependencyObject>())
+                    foreach (T c in FindLogicalChildren<T>(child))
+                        yield return c;
+            }
+        }
+    }
+
+}
